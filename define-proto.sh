25e619cd213cb16b04cfb53250bb61b2ac0eb91b
C H <litvintech@gmail.com>
2024-04-17 15:13:42 +0800

    Updated get and define proto scripts

--- define-proto.sh
+++ define-proto.sh
@@ -2,7 +2,7 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
-CYBER_REF='master'
+CYBER_REF='2.0.0'
 ROOT_PROTO_DIR="./proto/cyber/go-cyber-$CYBER_REF"
 CYBER_PROTO_DIR="$ROOT_PROTO_DIR/proto"
 THIRD_PARTY_PROTO_DIR="$ROOT_PROTO_DIR/third_party/proto"
@@ -17,7 +17,7 @@ protoc \
   --proto_path="$CYBER_PROTO_DIR" \
   --proto_path="$THIRD_PARTY_PROTO_DIR" \
   --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=messages" \
-  "$THIRD_PARTY_PROTO_DIR/cosmos_proto/pagination.proto" \
+  "$THIRD_PARTY_PROTO_DIR/cosmos/base/query/v1beta1/pagination.proto" \
   "$CYBER_PROTO_DIR/cyber/base/query/v1beta1/pagination.proto" \
   "$CYBER_PROTO_DIR/cyber/graph/v1beta1/types.proto" \
   "$CYBER_PROTO_DIR/cyber/graph/v1beta1/query.proto" \
--- get-proto.sh
+++ get-proto.sh
@@ -5,11 +5,10 @@ command -v shellcheck >/dev/null && shellcheck "$0"
 PROTO_DIR="./proto"
 CYBER_DIR="$PROTO_DIR/cyber"
 ZIP_FILE="$CYBER_DIR/tmp.zip"
-CYBER_REF='master'
-
+CYBER_REF='v2.0.0'
 mkdir -p "$CYBER_DIR"
 
-wget -qO "$ZIP_FILE" "https://github.com/cybercongress/go-cyber/archive/$CYBER_REF.zip"
+wget -qO "$ZIP_FILE" "https://github.com/cybercongress/go-cyber/archive/refs/tags/$CYBER_REF.zip"
 unzip "$ZIP_FILE" "*.proto" -d "$CYBER_DIR"
 rm "$ZIP_FILE"
 
